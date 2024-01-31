25fbb5ab09239f687b65c2364bc007e5ef24e74d
xiran lin <xiranlin@uw.edu>
2024-01-30 16:12:20 -0800

    111

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
-"""
-Change this to a world clock app
-"""
-
+import streamlit as st
+import pytz
+from datetime import datetime
 import time
 
-import streamlit as st
+# List of time zones
+time_zones = list(pytz.all_timezones)
 
-placeholder = st.empty()
+st.title("World Clock App")
 
-cnt = 0
+# Multi-select dropdown for time zones
+selected_zones = st.multiselect("Choose up to 4 time zones", time_zones, default=["UTC"])
+
+# Placeholder for clocks
+clocks_container = st.empty()
+
 while True:
-    with placeholder.container():
-        placeholder.metric("Seconds since you arrived this page", cnt)
-        cnt += 1
+    with clocks_container.container():
+        # Display clocks
+        for zone in selected_zones:
+            tz = pytz.timezone(zone)
+            time_now = datetime.now(tz).strftime('%Y-%m-%d %H:%M:%S')
+            st.metric(zone, time_now)
     time.sleep(1)
